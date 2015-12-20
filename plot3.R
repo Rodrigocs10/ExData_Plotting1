61c2c9befc9563f27375bfc4772e492b77f96ae6
Rodrigocs10 <rodrigocs10@gmail.com>
2015-12-20 17:17:08 -0300

    First version

---
+++ plot3.R
@@ -0,0 +1,16 @@
+cols <- c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+          "Voltage" , "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+consumption <- read.csv("household_power_consumption.txt", sep = ";", 
+                        na.strings = "?", skip = 66636, nrows = 2880, col.names = cols)
+library(datasets)
+
+## Draw
+
+with(consumption, plot(Sub_metering_2, Sub_metering_2, ylab = "Global Active Power (kilowatts)",
+                       xlab = "", type = "l"))
+with(subset(consumption))
+
+dev.copy(png, file = "plot3.png")
+
+dev.off()
+
